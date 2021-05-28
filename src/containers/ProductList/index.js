fcb0e2832b24ce6758317775180db351416814dc
Sami <Dev>
2021-05-28 18:34:45 +0000

    product list skeleton

--- src/containers/ProductList/index.js
+++ src/containers/ProductList/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import ProductItem from './../../components/ProductItem';
+import ProductItemSkeleton from './../../components/ProductItemSkeleton';
 import './ProductList.scss'
 import firebase from 'firebase/app';
 import 'firebase/firestore';
@@ -20,11 +21,15 @@ const ProductList = () =>{
             });
     },[tees])
 
+    const items =  products.map((product, index) => (
+        <ProductItem key={index} product={product} />
+    ))
+
+    const skeleton = <><ProductItemSkeleton /> <ProductItemSkeleton /><ProductItemSkeleton /><ProductItemSkeleton /><ProductItemSkeleton /><ProductItemSkeleton /><ProductItemSkeleton /><ProductItemSkeleton /><ProductItemSkeleton /><ProductItemSkeleton /><ProductItemSkeleton /></>
     return(
         <div className="product-list">
-            {products.map((product, index) => (
-                <ProductItem key={index} product={product} />
-            ))}
+            {skeleton}
+            {/* { products.length === 0 ? skeleton : items } */}
         </div>
     )
 }

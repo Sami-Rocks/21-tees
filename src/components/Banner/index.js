6a52d770c54e6963b0f0f5cdf77cd473b0b776d7
Sami <Dev>
2021-05-31 19:02:37 +0000

    upload Button

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import './Banner.scss'
+import { useHistory } from 'react-router';
+
 
 const Banner = () =>{
+
+    const history = useHistory();
+
+    const navigate= ()=> {
+        history.push("/custom-design");
+      }
+      
+
     return(
         <div className="banner">
             <div className="container">
@@ -10,7 +20,7 @@ const Banner = () =>{
                         DO YOU HAVE YOUR<br/>
                         OWN DESIGN?
                     </h1>
-                    <button className="button primary-button" >
+                    <button className="button primary-button" onClick={navigate} >
                         SHOW US
                     </button>
                 </div>
